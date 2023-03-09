e636bffc15d40424f816a208f4cc50a5228d7c96
Xudiyef21 <101468836+Xudiyef21@users.noreply.github.com>
2023-03-10 01:55:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// let vacancieDetails = document.querySelector('.cards')
+
+
+// vacancieDetails.addEventListener('click', function () {
+//     window.location.href = 'vacancies_details.html'
+// })
+
